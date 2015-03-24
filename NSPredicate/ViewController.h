86204e507c5b5270673dc49f4d2ae74b351b0623
jamalping <silvercreek@Ryans-MBA.local>
2015-03-24 11:41:26 +0800

    Initial Commit

---
+++ NSPredicate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSPredicate
+//
+//  Created by jamalping on 15-3-24.
+//  Copyright (c) 2015年 jamalping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
